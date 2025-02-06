72f3abb6b7b85028065f740a7e50e22c39a5181f
gabrielsivlvi <gabrielsv3000@hotmail.com>
2025-02-06 15:46:49 -0300

    ajustes finais

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Count, Avg
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
@@ -16,3 +18,24 @@ class MovieReatriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+
+class MovieStatsView(views.APIView):
+    #permission_classes =(IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+        
+        return response.Response(
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_reviews': total_reviews,
+                'average_stars': round(average_stars,2) if average_stars else 0,  
+                },
+            status=status.HTTP_200_OK
+            )
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
@@ -11,8 +10,8 @@ class ReviewCreateListView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+
 class ReviewRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
